d01368b075eaa682f003124c605c9897ea83a9c7
prashanthi-gowrisankar <70684780+prashanthi-gowrisankar@users.noreply.github.com>
2025-09-01 18:58:46 +0530

    Update database.py

--- my_backend/database.py
+++ my_backend/database.py
@@ -1,10 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
+load_dotenv()
 # Change values as per your PostgreSQL setup
-DATABASE_URL = "postgresql://juhomi:yourpassword@localhost:5432/mydb"
-
+# DATABASE_URL = "postgresql://juhomi:yourpassword@localhost:5432/mydb"
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
